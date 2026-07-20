5037039f9e5e5e834d87f17c74a7dee3b119a364
Bishal Sutradhar <bishalsutradhar2006@gmail.com>
2026-07-20 22:23:48 +0530

    Add password handling to user registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,19 @@
 const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
 const register = async (req, res) => {
 
-    const { username, email } = req.body
-
+    const { username, email, password } = req.body
+    const hashedPassword = bcrypt.hash(
+        password,
+        10
+    )
+    
     const user = await userModel.create({
         username,
-        email
+        email,
+        password: hashedPassword
     })
 
     res.status(201).json({
