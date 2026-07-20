b9815e98592339c589a558f523cf0099aa817091
Bishal Sutradhar <bishalsutradhar2006@gmail.com>
2026-07-21 01:47:49 +0530

    Add error handling and stop duplicate user accouts creation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,20 +5,47 @@ const bcrypt = require("bcrypt")
 const register = async (req, res) => {
 
     const { username, email, password } = req.body
-    const hashedPassword = bcrypt.hash(
-        password,
-        10
-    )
-    
-    const user = await userModel.create({
-        username,
-        email,
-        password: hashedPassword
+
+    const user = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
     })
 
-    res.status(201).json({
-        message: "User registered successfully!"
-    })
+    if(user) {
+        console.log("User already exists!")
+
+        return res.status(409).json({
+            message: "User already exists!"
+        })
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(
+            password,
+            10
+        )
+        
+        const user = await userModel.create({
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        res.status(201).json({
+            message: "User registered successfully!"
+        })
+    } catch(err) {
+        console.error("User registration failed!")
+
+        return res.status(400).json({
+            message: "User registration failed!",
+            error: err.message
+        })
+    }
+
+    
 
 }
 
