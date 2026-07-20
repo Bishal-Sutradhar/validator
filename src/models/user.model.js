94975a887392cf5d77f9555ad3fb807780d56186
Bishal Sutradhar <bishalsutradhar2006@gmail.com>
2026-07-20 22:31:19 +0530

    Add required validation for username and password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,12 +4,17 @@ const userSchema = new mongoose.Schema({
 
     username: {
         type: String,
+        required: true,
         unique: true
     },
     email: {
         type: String,
         required: true
-    }
+    },
+    password: {
+        type: String,
+        required: true
+    },
 })
 
 const userModel = mongoose.model("user", userSchema)
